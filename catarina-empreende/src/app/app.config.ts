394ceb5c808bf4f6704bc036d94560c4b7fab1d0
Leandro Prado Pires <leandropradopires@MacBook-Pro-de-Leandro.local>
2026-03-04 19:28:41 -0300

    feat(firebase): ConfiguraFirebase no projeto

--- catarina-empreende/src/app/app.config.ts
+++ catarina-empreende/src/app/app.config.ts
@@ -5,6 +5,9 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +24,8 @@ export const appConfig: ApplicationConfig = {
         }
       }
     }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ]
 };
